dca2fa61ad17d809997d2ceca8ccc645e88857d2
Md Zahirul Haque <116150226+zahir1206@users.noreply.github.com>
2023-11-27 02:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const makeChange = (c) => {
   // your name here
+	let output={};
+	for(let i in units){
+		let currentUnit=units[i];
+		output[i]=parseInt(c/currentUnit);
+		c=c-currentUnit*output[i];
+	}
+	console.log(output);
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
